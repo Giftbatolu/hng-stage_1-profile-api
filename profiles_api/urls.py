840030fe53ce8f44c4d91ce97576f8ee603f734e
giftbatolu <sekinatoyelami@gmail.com>
2026-04-27 22:49:51 +0100

    add natural query seach to the api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProfileListCreateView, ProfileDetailView
+from .views import ProfileListCreateView, ProfileDetailView, NaturalQueryView
 
 urlpatterns = [
     path("profiles", ProfileListCreateView.as_view()),
     path("profiles/<uuid:id>", ProfileDetailView.as_view()),
+    path("profiles/search", NaturalQueryView.as_view()),
 ]
